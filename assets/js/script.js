b50090e773603d554518073f0862db461bce0ca4
chieby-mwangi <168543322+chieby-mwangi@users.noreply.github.com>
2024-06-13 11:21:53 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Mwangi Brian";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/skills/");
         }
         else {
             document.title = "Come Back To Portfolio";
