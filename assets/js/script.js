0ef4f8dd7ae8f12a91991a2aef2b33f7267216b0
mwangi <chiebyrah@gmail.com>
2024-06-15 10:05:01 +0300

    updated the assetsjs file

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development",],
+    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development", "Graphics Design"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
